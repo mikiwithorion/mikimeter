7cead2be2bfddc5f6698c37f9b1322e1740b7186
mikiwithorion <miki.riku.haru@gmail.com>
2025-04-23 12:40:21 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="みきメーター", layout="centered")
+
+st.title("みきメーター")
+st.write("ようこそ！これは、みき専用の進捗＆やる気メーターです！！")
+
+progress = st.slider("今日の達成度は？", 0, 100, 50)
+st.progress(progress)
+
+if progress == 100:
+    st.balloons()
+    st.success("うおおおお！！！今日も最高！！！！！！！！！！！！")
